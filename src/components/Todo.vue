f37801ff4bc5647ca37a05124a56d7bc473b05ec
kuranuki <kuranuki@sonicgarden.jp>
2017-06-17 15:46:11 +0900

    TODO追加と削除

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="todo">
-    <h1>{{ msg }}</h1>
+
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="9">
+        <h1>{{ msg }}</h1>
+
+        <el-input placeholder="何する？" v-model="newtodo"></el-input>
+        <el-button @click="addTodo">追加</el-button>
+
+        <ul id="todos">
+          <li v-for="todo in todos">
+            {{ todo.item }}
+            <i class="el-icon-delete todo-delete" @click="deleteTodo(todo)"></i>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
+
     <h2>Essential Links</h2>
     <i class="el-icon-edit"></i>
     <i class="el-icon-share"></i>
@@ -14,7 +30,30 @@ export default {
   name: 'todo',
   data () {
     return {
-      msg: 'Todo App'
+      msg: 'Todo App',
+      newtodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo: function (event) {
+      event.preventDefault()
+      if (this.newtodo === '') return
+      this.todos.push({
+        item: this.newtodo
+      })
+      this.newtodo = ''
+    },
+    deleteTodo: function (todo) {
+      var index = this.todos.indexOf(todo)
+      this.todos.splice(index, 1)
+    },
+    open () {
+      const h = this.$createElement
+      this.$notify({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder')
+      })
     }
   }
 }
@@ -22,4 +61,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.todo {
+  text-align: left;
+}
+
+.todo-delete {
+  font-size: 8px;
+  cursor: pointer;
+}
 </style>
