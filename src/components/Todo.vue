297734f2cf75fd8ed6fe38e573ff3fa1909019b0
kuranuki <kuranuki@sonicgarden.jp>
2017-06-17 10:14:30 +0900

    TODOアプリ枠

---
+++ src/components/Todo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="todo">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="search">Search</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      msg: 'Todo App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
