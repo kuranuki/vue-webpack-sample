45a471b8412ffc76fa516d09ad59c2af7b80b15f
倉貫義人 <kuranuki@kura-mac.local>
2017-06-17 10:00:16 +0900

    add to use element

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,6 +17,10 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <h2>component</h2>
+    <div class="block">
+      <el-color-picker v-model="color"></el-color-picker>
+    </div>
   </div>
 </template>
 
@@ -25,7 +29,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      color: '#20a0ff'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import locale from 'element-ui/lib/locale/lang/ja'
+
+Vue.use(ElementUI, { locale })
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
